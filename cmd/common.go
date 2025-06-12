78770ed27e6ded9a080a189fcd677ef902a4df98
萝莉璃 <49371494+luolili233@users.noreply.github.com>
2025-06-12 13:04:11 +0800

    Update common.go

--- cmd/common.go
+++ cmd/common.go
@@ -5,10 +5,10 @@ import (
 	"path/filepath"
 	"strconv"
 
-	"github.com/alist-org/alist/v3/internal/bootstrap"
-	"github.com/alist-org/alist/v3/internal/bootstrap/data"
-	"github.com/alist-org/alist/v3/internal/db"
-	"github.com/alist-org/alist/v3/pkg/utils"
+	github.com/NewAlist/alist/internal/bootstrap"
+	"github.com/NewAlist/alist/internal/bootstrap/data"
+	"github.com/NewAlist/alist/internal/db"
+	"github.com/NewAlist/alist/pkg/utils"
 	log "github.com/sirupsen/logrus"
 )
 
